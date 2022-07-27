9c61c817714dd9175915a0c5bcce47e09f66c10a
daniela <d.ortizkosobucki@gmail.com>
2022-07-27 12:02:47 -0400

    correct rendering on navigation link

--- social-e-cards/src/App.js
+++ social-e-cards/src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>HELLO PEOPLE</h1>
-      {/* <Profile /> */}
-      <Navigation loggedIn={true} />
+      <Navigation areYouLoggedIn={false} />
       <Profile />
     </div>
   );
--- social-e-cards/src/components/navigation.js
+++ social-e-cards/src/components/navigation.js
@@ -2,35 +2,66 @@ import React, { useEffect } from "react";
 import NavigationLink from "./nav-link";
 import { useState } from "react";
 
-function Navigation(props) {
+function Navigation({ areYouLoggedIn }) {
     const [loggedIn, setLoggedIn] = useState("Log In")
+
     useEffect(() => {
-        if (props.loggedIn === true) {
+        if (areYouLoggedIn === true) {
             setLoggedIn("Log Out")
+            console.log("this is true")
         } else {
             setLoggedIn("Log In")
+            console.log("this is false")
         }
-    }, [props.loggedIn])
+    }, [areYouLoggedIn])
 
     return (
         <>
-            <h1>HELLO AGAIN</h1>
-            <NavigationLink text={loggedIn} link="/auth/token/login/" />
-            {props.loggedIn ? (
+            <h1>HELLO AGAIN haha</h1>
+            {areYouLoggedIn ? (
                 <>
                     <NavigationLink text="Profile " link="/auth/users/me/" />
                     <NavigationLink text="Create Cards " link="/create-card/" />
+                    <NavigationLink text="Log Out" link="/auth/token/logout/" />
                 </>
             ) : (
                 <>
-                    <Navigation text="Create Account" link="/auth/users/" />
+                    <NavigationLink text="Log In" link="/auth/token/login/" />
+                    <NavigationLink text="Create Account" link="/auth/users/" />
                 </>
             )
             }
-            {/* <NavigationLink text="Create Cards " link="/create-cards" /> */}
         </>
     );
 }
 
 export default Navigation
+
+// function Navigation() {
+//     const [loggedIn, setLoggedIn] = useState("Log In")
+//     const [areYouLoggedIn, setAreYouLoggedIn] = useState(true)
+
+//     return (
+//         <>
+//             <h1>HELLO AGAIN haha</h1>
+//             <button onClick={() => setAreYouLoggedIn(!areYouLoggedIn)}>LOG IN?!</button>
+//             {/* <NavigationLink text="Log Out" link="/auth/token/login/" /> */}
+//             {areYouLoggedIn ? (
+//                 <>
+//                     <NavigationLink text="Profile " link="/auth/users/me/" />
+//                     <NavigationLink text="Create Cards " link="/create-card/" />
+//                     <NavigationLink text="Log Out" link="/auth/token/logout/" />
+//                 </>
+//             ) : (
+//                 <>
+//                     <NavigationLink text="Log In" link="/auth/token/login/" />
+//                     <NavigationLink text="Create Account" link="/auth/users/" />
+//                 </>
+//             )
+//             }
+//         </>
+//     );
+// }
+
+// export default Navigation
 
