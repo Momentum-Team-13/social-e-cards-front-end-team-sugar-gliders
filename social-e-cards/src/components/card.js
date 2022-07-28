5eddd12bf226f89e4a7caa5dc245dcc9151df02d
moperry2 <moperrry@gmail.com>
2022-07-28 09:37:57 -0400

    added react hook

--- social-e-cards/src/components/card.js
+++ social-e-cards/src/components/card.js
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Card.css";
 // import ReactCardFlip from "react-card-flip";
 // adding comment
 const Card = (props) => {
   const { key, color, outmessage, inmessage } = props;
   const [flip, setFlip] = useState(false);
-
+  const frontEl = useRef();
+  const backEl = useRef();
   return (
     <>
       <div className="card" id={key}>
-        <div
-          className={`card ${flip ? "flip" : ""}`}
-          onClick={() => setFlip(!flip)}
-        >
-          <div className="front" ref={color}>
-            {outmessage}
-            {/* <img src={image} /> */}
-          </div>
-          <div className="back" ref={color}>
-            {inmessage}
+        <div classname={`${color}`}>
+          <div
+            className={`card ${flip ? "flip" : ""}`}
+            onClick={() => setFlip(!flip)}
+          >
+            <div className="front" ref={frontEl}>
+              {outmessage}
+              {/* <img src={image} /> */}
+            </div>
+            <div className="back" ref={backEl}>
+              {inmessage}
+            </div>
           </div>
         </div>
       </div>
