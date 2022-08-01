83c8fef79b09592dc6a037c5038886d61dd14151
moperry2 <moperrry@gmail.com>
2022-08-01 18:25:53 -0400

    delete button working only on card's user creates

--- social-e-cards/src/components/completeCard.js
+++ social-e-cards/src/components/completeCard.js
@@ -1,67 +1,87 @@
 import React, { useState, useRef } from "react";
-import "./css/card.css"
+import "./css/card.css";
+import axios from "axios";
 import Navigation from "./navigation";
 import "bulma/css/bulma.min.css";
 // import ReactCardFlip from "react-card-flip";
 import "./css/card.css";
-
+let Token = localStorage.getItem("auth_token");
 // adding comment
 export default function Card(props) {
-    const { id, color, index, outmessage, inmessage, img } = props;
-    // const [isFlipped, setIsFlipped] = useState(false);
-    const frontEl = useRef();
-    const backEl = useRef();
-    console.log(props)
+  const { id, color, index, outmessage, inmessage, image } = props;
+  // const [isFlipped, setIsFlipped] = useState(false);
+  const frontEl = useRef();
+  const backEl = useRef();
+  console.log(props.image);
+  console.log(props);
 
-    const CardStyle = {
-        border: "1px solid black",
-        padding: "20px",
-        margin: "20px",
-        width: "200px",
-        height: "300px",
-        backgroundColor: `#${props.color}`
-    };
-
-    return (
-        <>
-            <br />
-            {/* <Navigation /> */}
-            <br />
-            {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal"> */}
-            <div
-                style={CardStyle}
-                // onMouseEnter={() => setIsFlipped((prev) => !prev)}
-                className="CardFront"
-            >
-                <div className="card" id={id} key={index}>
-                    <div className={`${color}`}>
-                        <div
-                        // className={`card ${flip ? "flip" : ""}`}
-                        // onClick={() => setFlip(!flip)}
-                        >
-                            <div className="front" ref={frontEl}>
-                                {inmessage}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div
-                style={CardStyle}
-                // onMouseLeave={() => setIsFlipped((prev) => !prev)}
-                className="CardBack"
-            >
-                <div className="back" ref={backEl}>
-                    {outmessage}
-                    <div className="image">
-                        <img src={img} alt="" />
-                    </div>
-                </div>
-            </div>
-            {/* </ReactCardFlip> */}
-            {/* <Navigation className="bottom-nav" /> */}
-
-        </>
+  const CardStyle = {
+    border: "1px solid black",
+    padding: "20px",
+    margin: "20px",
+    width: "200px",
+    height: "300px",
+    backgroundColor: `#${props.color}`,
+  };
+  const deleteCard = (event) => {
+    // event.preventDefault();
+    console.log(event.target.id);
+    axios.delete(
+      `https://sg-ecard-api.herokuapp.com/ecards/${event.target.id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${Token}`,
+        },
+      }
     );
-};
-
+    const element = document.getElementById(event.target.id);
+    element.remove();
+  };
+  return (
+    <>
+      <div id={id}>
+        <br />
+        {/* <Navigation /> */}
+        <br />
+        {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal"> */}
+        <div
+          style={CardStyle}
+          // onMouseEnter={() => setIsFlipped((prev) => !prev)}
+          className="CardFront"
+        >
+          <div className="card" id={id} key={index}>
+            <div className={`${color}`}>
+              <div
+              // className={`card ${flip ? "flip" : ""}`}
+              // onClick={() => setFlip(!flip)}
+              >
+                <div className="front" ref={frontEl}>
+                  {inmessage}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          style={CardStyle}
+          // onMouseLeave={() => setIsFlipped((prev) => !prev)}
+          className="CardBack"
+        >
+          <div className="back" ref={backEl}>
+            {outmessage}
+            <div className="image">
+              <img src={props.image} alt="" />
+            </div>
+          </div>
+          {/* Need to only show delete button for cards created by the user */}
+          <button type="submit" id={id} onClick={(event) => deleteCard(event)}>
+            Delete Card
+          </button>
+        </div>
+        {/* </ReactCardFlip> */}
+        {/* <Navigation className="bottom-nav" /> */}
+      </div>
+    </>
+  );
+}
--- social-e-cards/src/components/galleryCards.js
+++ social-e-cards/src/components/galleryCards.js
@@ -1,65 +1,64 @@
 import Card from "./completeCard";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "./css/card.css"
+import "./css/card.css";
 import Navigation from "./navigation";
 import "bulma/css/bulma.min.css";
 
-
 const Gallery = () => {
-    const [cards, setCards] = useState(null);
-    let token = localStorage.getItem("auth_token");
+  const [cards, setCards] = useState(null);
+  let token = localStorage.getItem("auth_token");
 
-    useEffect(() => {
-        axios
-            .get("https://sg-ecard-api.herokuapp.com/ecards/", {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Token ${token}`,
-                },
-            })
-            .then((res) => setCards(res.data));
-    }, [setCards]);
+  useEffect(() => {
+    axios
+      .get("https://sg-ecard-api.herokuapp.com/ecards/", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => setCards(res.data));
+  }, [setCards]);
 
-    return (
-        <>
-            <br />
-            <h1 className="app-name">Gliding Sugar Cards</h1>
-            <br />
-            <Navigation />
-            <br />
-            <div className="gallery_container">
-                <header className="App-header">
-                    <h1>Welcome Greeter!!</h1>
-                </header>
-                <div className="cards_container">
-                    <div className="card_grid">
-                        {cards &&
-                            cards.map((card, index) => {
-                                return (
-                                    <Card
-                                        id={card.id}
-                                        color={card.card_color}
-                                        key={index}
-                                        // msgfont={card.outer_font}
-                                        outmessage={card.card_outer_message}
-                                        inmessage={card.card_inner_message}
-                                        image={card.card_image}
-                                    // creator={card.author}
-                                    // created={card.date_created}
-                                    />
-                                );
-                            })}
-                    </div>
-                </div>
-            </div>
-            <br />
-            <Navigation />
-            <br />
-        </>
-    );
+  return (
+    <>
+      <br />
+      <h1 className="app-name">Gliding Sugar Cards</h1>
+      <br />
+      <Navigation />
+      <br />
+      <div className="gallery_container">
+        <header className="App-header">
+          <h1>Welcome Greeter!!</h1>
+        </header>
+        <div className="cards_container">
+          <div className="card_grid">
+            {cards &&
+              cards.map((card, index) => {
+                return (
+                  <Card
+                    id={card.id}
+                    color={card.card_color}
+                    key={index}
+                    // msgfont={card.outer_font}
+                    outmessage={card.card_outer_message}
+                    inmessage={card.card_inner_message}
+                    image={card.card_image}
+                    // creator={card.author}
+                    // created={card.date_created}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </div>
+      <br />
+      <Navigation />
+      <br />
+    </>
+  );
 };
 export default Gallery;
 
-//delete navigation from the card component 
+//delete navigation from the card component
 // add in who created the card to pass data
