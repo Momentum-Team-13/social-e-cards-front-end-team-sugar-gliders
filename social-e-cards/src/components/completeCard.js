dacaaa96be487c25b6061b8a971cc74c8831dde9
moperry2 <moperrry@gmail.com>
2022-08-04 11:12:34 -0400

    updated read me, create card page, and styling for a single card

--- social-e-cards/src/components/completeCard.js
+++ social-e-cards/src/components/completeCard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./css/card.css"
+import "./css/card.css";
 import Navigation from "./navigation";
 import "bulma/css/bulma.min.css";
 import { Link } from "react-router-dom";
@@ -7,185 +7,182 @@ import axios from "axios";
 
 // adding comment
 export default function Card(props) {
-    const { id, color, index, outmessage, inmessage, img, owner, following, ownerID, followerCardID, cardCreator } = props;
-    const frontEl = useRef();
-    const backEl = useRef();
-    let token = localStorage.getItem("auth_token");
-    const [deleteID, setDeleteID] = useState(null)
-    const [currentFollowers, setCurrentFollowers] = useState(following)
-    console.log(cardCreator)
+  const {
+    id,
+    color,
+    index,
+    outmessage,
+    inmessage,
+    img,
+    owner,
+    following,
+    ownerID,
+    followerCardID,
+    cardCreator,
+  } = props;
+  const frontEl = useRef();
+  const backEl = useRef();
+  let token = localStorage.getItem("auth_token");
+  const [deleteID, setDeleteID] = useState(null);
+  const [currentFollowers, setCurrentFollowers] = useState(following);
+  console.log(cardCreator);
 
+  //   const CardStyleOutside = {
+  //     border: "3px solid black",
+  //     padding: "40px",
+  //     margin: "20px",
+  //     width: "300px",
+  //     height: "150px",
+  //     backgroundColor: `#${props.color}`,
+  //   };
 
-    const CardStyleOutside = {
-        border: "3px solid black",
-        padding: "40px",
-        margin: "20px",
-        width: "300px",
-        height: "150px",
-        backgroundColor: `#${props.color}`
-    };
+  const CardStyle = {
+    border: "2px solid black",
+    padding: "20px",
+    margin: "20px",
+    width: "300px",
+    height: "400px",
+    backgroundColor: `#${props.color}`,
+  };
 
-    const CardStyleInside = {
-        border: "2px solid black",
-        padding: "20px",
-        margin: "20px",
-        width: "300px",
-        height: "350px",
-        backgroundColor: `#${props.color}`
-    };
+  useEffect(() => {
+    axios
+      .get("https://sg-ecard-api.herokuapp.com/followers/", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then((res) => {
+        let array = [];
+        res.data.forEach((element) => {
+          if (element.following === ownerID) {
+            setDeleteID(element.id);
+          }
+        });
+      })
+      .catch((res) => {
+        // let error = res.response.data.non_field_errors;
+        // console.log(error);
+        // setError(error);
+      });
+  }, [currentFollowers, token]);
 
-    useEffect(() => {
-        axios
-            .get('https://sg-ecard-api.herokuapp.com/followers/',
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Token ${token}`,
-                    },
-                })
-            .then((res) => {
-                let array = []
-                res.data.forEach(element => {
-                    if (element.following === ownerID) {
-                        setDeleteID(element.id)
-                    }
-                });
+  const handleFollowRequest = (event) => {
+    axios
+      .post(
+        "https://sg-ecard-api.herokuapp.com/followers/",
+        {
+          following: ownerID,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token ${token}`,
+          },
+        }
+      )
+      .then(() => {
+        const newFollowers = following.slice(0);
+        newFollowers.push(ownerID);
+        setCurrentFollowers(newFollowers);
+      });
+  };
 
-            })
-            .catch((res) => {
-                // let error = res.response.data.non_field_errors;
-                // console.log(error);
-                // setError(error);
-            })
-    }, [currentFollowers, token]);
+  const handleUnfollowRequest = (event) => {
+    axios
+      .delete(`https://sg-ecard-api.herokuapp.com/followers/${deleteID}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      })
+      .then(() => {
+        const index = following.indexOf(deleteID);
+        const newFollowers = following.slice(0);
+        newFollowers.splice(index, 1);
+        setCurrentFollowers(newFollowers);
+        console.log(newFollowers);
+      });
+  };
 
-    const handleFollowRequest = (event) => {
-        axios
-            .post('https://sg-ecard-api.herokuapp.com/followers/', {
-                following: ownerID
-            },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Token ${token}`,
-                    },
-                }
-            )
-            .then(() => {
-                const newFollowers = following.slice(0)
-                newFollowers.push(ownerID)
-                setCurrentFollowers(newFollowers)
-            }
-
-            )
-    }
-
-    const handleUnfollowRequest = (event) => {
-
-        axios
-            .delete(`https://sg-ecard-api.herokuapp.com/followers/${deleteID}`, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Token ${token}`,
-                },
-            })
-            .then(() => {
-                const index = following.indexOf(deleteID)
-                const newFollowers = following.slice(0)
-                newFollowers.splice(index, 1)
-                setCurrentFollowers(newFollowers)
-                console.log(newFollowers)
-            }
-            );
-    }
-
-    const deleteCard = (event) => {
-        event.preventDefault();
-        console.log(event.target.id);
-        axios
-            .delete(
-                `https://sg-ecard-api.herokuapp.com/ecards/${event.target.id}`,
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Token ${token}`,
-                    },
-                }
-            );
-        const element = document.getElementById(event.target.id);
-        element.remove();
-        window.location.reload();
-    };
-
-    return (
-        <>
-            <br />
-            {/* <Navigation /> */}
-            <br />
-            {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal"> */}
-            <div className="wholeCard">
-                <div
-                    style={CardStyleOutside}
-                    // onMouseEnter={() => setIsFlipped((prev) => !prev)}
-                    className="CardFront"
-                >
-
-                    <div id={id} key={index} className={`${color}`}>
-                        <div
-                        // className={`card ${flip ? "flip" : ""}`}
-                        // onClick={() => setFlip(!flip)}
-                        >
-                            <div className="front" ref={frontEl}>
-                                {outmessage}
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div
-                    style={CardStyleInside}
-                    // onMouseLeave={() => setIsFlipped((prev) => !prev)}
-                    className="CardBack"
-                >
-                    <div className="back" ref={backEl}>
-                        {inmessage}
-                        <div className="image">
-                            <img src={img} alt="" />
-                            <h3>Created by: {cardCreator}</h3>
-                            {owner ?
-                                (
-                                    <>
-                                        <button
-                                            type="submit"
-                                            id={id}
-                                            onClick={(event) => deleteCard(event)}
-                                        // onClick={() => window.location.reload()}
-                                        >
-                                            Delete Card
-                                        </button>
-                                        <button
-                                            type="submit"
-                                            id={id}
-                                        >
-                                            <Link to={`/edit/${id}`}>Edit</Link>
-                                        </button>
-                                    </>
-                                ) : (
-                                    ""
-                                )}
-                            {(currentFollowers.includes(ownerID) && (!owner)) ? (<button onClick={() => handleUnfollowRequest()}>Unfollow User </button>)
-                                : (!currentFollowers.includes(ownerID) && (!owner)) ? (<button onClick={() => handleFollowRequest()}>Follow User </button>)
-                                    : ""
-                            }
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/* </ReactCardFlip> */}
-            {/* <Navigation className="bottom-nav" /> */}
-
-        </>
+  const deleteCard = (event) => {
+    event.preventDefault();
+    console.log(event.target.id);
+    axios.delete(
+      `https://sg-ecard-api.herokuapp.com/ecards/${event.target.id}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+      }
     );
-};
+    const element = document.getElementById(event.target.id);
+    element.remove();
+    window.location.reload();
+  };
+  //    flip = document.querySelector("#myCard").classList.toggle("flip");
 
+  return (
+    <>
+      <br />
+      {/* <Navigation /> */}
+      <br />
+
+      <div
+        class="flip-container"
+        ontouchstart="this.classList.toggle('hover');"
+      >
+        {/* <div className="wholeCard"> */}
+        <div style={CardStyle} className="CardFront">
+          <div class="flipper">
+            <div className="front" ref={frontEl}>
+              <div id={id} key={index} className={`${color}`}>
+                <h2> {outmessage} </h2>
+                {/* <div style={CardStyle} className="CardBack">
+                  <div className="back" ref={backEl}> */}
+                <h3> {inmessage} </h3>
+                <div className="image">
+                  <img src={img} alt="" />
+                  <h4>Created by: {cardCreator}</h4>
+                  {owner ? (
+                    <>
+                      <button
+                        type="submit"
+                        id={id}
+                        onClick={(event) => deleteCard(event)}
+                        // onClick={() => window.location.reload()}
+                      >
+                        Delete Card
+                      </button>
+                      <button type="submit" id={id}>
+                        <Link to={`/edit/${id}`}>Edit</Link>
+                      </button>
+                    </>
+                  ) : (
+                    ""
+                  )}
+                  {currentFollowers.includes(ownerID) && !owner ? (
+                    <button onClick={() => handleUnfollowRequest()}>
+                      Unfollow User{" "}
+                    </button>
+                  ) : !currentFollowers.includes(ownerID) && !owner ? (
+                    <button onClick={() => handleFollowRequest()}>
+                      Follow User{" "}
+                    </button>
+                  ) : (
+                    ""
+                  )}
+                </div>
+              </div>
+            </div>
+            {/* {/* </div> */}
+            {/* </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- social-e-cards/src/components/createCard.js
+++ social-e-cards/src/components/createCard.js
@@ -3,139 +3,116 @@ import { useState } from "react";
 import Navigation from "./navigation";
 import { TwitterPicker } from "react-color";
 import rgbHex from "rgb-hex";
-import { Navigate, useNavigate } from "react-router-dom";
-import 'bulma/css/bulma.min.css';
+import { useNavigate } from "react-router-dom";
+import "bulma/css/bulma.min.css";
 
 export default function CreateCard(username) {
-    let Token = localStorage.getItem("auth_token");
-    const [img, setImg] = useState("");
-    const [inmessage, setInnerMessage] = useState("");
-    const [outmessage, setOuterMessage] = useState("");
-    const [color, setColor] = useState("");
-    const [userId, setUserId] = useState("");
-    const [cardCreator, setCardCreator] = useState(null);
-    const returnProfile = useNavigate();
+  let Token = localStorage.getItem("auth_token");
+  const [img, setImg] = useState("");
+  const [inmessage, setInnerMessage] = useState("");
+  const [outmessage, setOuterMessage] = useState("");
+  const [color, setColor] = useState("");
+  const [cardCreator, setCardCreator] = useState(null);
+  const returnProfile = useNavigate();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    axios
+      .post(
+        "https://sg-ecard-api.herokuapp.com/ecards/",
+        {
+          card_inner_message: inmessage,
+          card_outer_message: outmessage,
+          card_image: img,
+          card_color: color,
+          card_owner: { username },
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Token ${Token}`,
+          },
+        }
+      )
+      .then(
+        (res) => {
+          setInnerMessage("");
+          setOuterMessage("");
+          setImg("");
+          setCardCreator(res.data.card_owner.username);
+          returnProfile("/profile");
+        },
+        [cardCreator]
+      );
+  };
 
-        axios
-            .get("https://sg-ecard-api.herokuapp.com/auth/users/me/", {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Token ${Token}`,
-
-                },
-            })
-            .then((res) => {
-                setUserId(res.id);
-            });
-
-        axios
-            .post(
-                "https://sg-ecard-api.herokuapp.com/ecards/",
-                {
-                    card_inner_message: inmessage,
-                    card_outer_message: outmessage,
-                    card_image: img,
-                    card_color: color,
-                    card_owner: { username }
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Token ${Token}`,
-                    },
-                }
-            )
-            .then((res) => {
-                setInnerMessage("");
-                setOuterMessage("");
-                setImg("");
-                setCardCreator(res.data.card_owner.username)
-                returnProfile("/profile")
-            }, [cardCreator]);
-    };
-
-
-    return (
-        <>
+  return (
+    <>
+      <br />
+      <h1 className="app-name">Gliding Sugar Cards</h1>
+      <br />
+      <Navigation />
+      <br />
+      <div id="cardform">
+        <h2>Customize your card!</h2>
+        <br />
+        <h3>Step 1: Choose a Color:</h3>
+        <br />
+        <TwitterPicker
+          color={color}
+          onChangeComplete={(c) => setColor(rgbHex(c.rgb.r, c.rgb.g, c.rgb.b))}
+        />
+        <p>You picked {color}</p>
+        <form onSubmit={handleSubmit} id="add-card">
+          <div className="input-field" id="card-message-field">
+            <label htmlFor="message">
+              Step 2: Write a Message for the Outside of the Card:
+            </label>
             <br />
-            <h1 className="app-name">Gliding Sugar Cards</h1>
+            <input
+              type="textarea"
+              value={outmessage}
+              name="message"
+              placeholder="Give your card a message!"
+              onChange={(e) => setOuterMessage(e.target.value)}
+            />
+          </div>
+          <div className="input-field" id="card-message-field">
+            <label htmlFor="message">
+              {" "}
+              Step 3: Write a Message for the Inside of the Card:
+            </label>
             <br />
-            <Navigation />
-            <br />
-            <div id="cardform">
-                <h2>Customize your card!</h2>
-                <br />
-                <h3>Step 1: Choose a Color:</h3>
-                <br />
-                <TwitterPicker
-                    color={color}
-                    onChangeComplete={(c) => setColor(rgbHex(c.rgb.r, c.rgb.g, c.rgb.b))}
-                />
-                <p>You picked {color}</p>
-                <form
-                    onSubmit={handleSubmit}
-                    id="add-card"
-                >
-                    <div className="input-field" id="card-message-field">
-                        <label htmlFor="message">Step 2: Write a Message for the Outside of the Card:</label>
-                        <br />
-                        <input
-                            type="textarea"
-                            value={outmessage}
-                            name="message"
-                            placeholder="Give your card a message!"
-                            onChange={(e) => setOuterMessage(e.target.value)}
-                        />
-                    </div>
-                    <div className="input-field" id="card-message-field">
-                        <label htmlFor="message"> Step 3: Write a Message for the Inside of the Card:</label>
-                        <br />
-                        <input
-                            type="textarea"
-                            value={inmessage}
-                            name="message"
-                            placeholder="Give your card a message!"
-                            onChange={(e) => setInnerMessage(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <div className="input-field" id="card-image-field">
-                            <label htmlFor="image"> Step 4: Insert Unsplash URL:</label>
-                            <br />
-                            <input
-                                type="textarea"
-                                value={img}
-                                name="image"
-                                placeholder="Enter URL here!"
-                                onChange={(e) => setImg(e.target.value)}
-                            />
-                        </div>
-                    </div>
-                    <br />
-                    <button
-                        type="submit"
-                        id="submit"
-                    >
-                        Done!
-                    </button>
-                    <br />
-                </form>
-
+            <input
+              type="textarea"
+              value={inmessage}
+              name="message"
+              placeholder="Give your card a message!"
+              onChange={(e) => setInnerMessage(e.target.value)}
+            />
+          </div>
+          <div>
+            <div className="input-field" id="card-image-field">
+              <label htmlFor="image"> Step 4: Insert Unsplash URL:</label>
+              <br />
+              <input
+                type="textarea"
+                value={img}
+                name="image"
+                placeholder="Enter URL here!"
+                onChange={(e) => setImg(e.target.value)}
+              />
             </div>
-            <br />
-
-        </>
-    );
+          </div>
+          <br />
+          <button type="submit" id="submit">
+            Done!
+          </button>
+          <br />
+        </form>
+      </div>
+      <br />
+    </>
+  );
 }
-
-
-//card created message upon submit
-//clear image after submit like the inner/outer message 
-// submit with user id in card create
-//call by user id cards created 
-
-
